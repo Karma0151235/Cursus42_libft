04918f95418f779936af07607aec020095133a3f
Stephen Tee <stephentee88@gmail.com>
2025-10-17 11:04:17 +0800

    Refactor ft_atoi function with header comments

--- libft/LIBC Functions/ft_atoi.c
+++ libft/LIBC Functions/ft_atoi.c
@@ -1,26 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stee <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/17 11:01:50 by stee              #+#    #+#             */
+/*   Updated: 2025/10/17 11:04:01 by stee             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-int ft_atoi(const char *nptr)
+int     ft_atoi(const char *nptr)
 {
-    int sum;
-    int sign;
-    int i;
+        int     sum;
+        int     sign;
+        int     i;
 
-    sum = 0;
-    sign = 1;
-    i = 0;
-    while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
-        i++;
-    if (nptr[i] == '+' || nptr[i] == '-')
-    {
-        if (nptr[i] == '-')
-            sign *= -1;
-        i++;
-    }
-    while (nptr[i] >= '0' && nptr[i] <= '9')
-    {
-        sum = (sum * 10) + (nptr[i] - '0');
-        i++;
-    }
-    return (sum * sign);
+        sum = 0;
+        sign = 1;
+        i = 0;
+        while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
+                i++;
+        if (nptr[i] == '+' || nptr[i] == '-')
+        {
+                if (nptr[i] == '-')
+                        sign *= -1;
+                i++;
+        }
+        while (nptr[i] >= '0' && nptr[i] <= '9')
+        {
+                sum = (sum * 10) + (nptr[i] - '0');
+                i++;
+        }
+        return (sum * sign);
 }
