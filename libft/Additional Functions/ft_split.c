aebf7eca4d1275e87c2b4bd2d904d71dca2e1d91
Stephen Tee <stephentee88@gmail.com>
2025-10-17 11:32:11 +0800

    Update ft_split.c

--- libft/Additional Functions/ft_split.c
+++ libft/Additional Functions/ft_split.c
@@ -1,85 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stee <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/17 11:09:53 by stee              #+#    #+#             */
+/*   Updated: 2025/10/17 11:31:16 by stee             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-static size_t ft_count_words(char const *s, char c)
+static size_t   ft_count_words(char const *s, char c)
 {
-    size_t i;
-    size_t ct;
+        size_t  i;
+        size_t  ct;
 
-    i = 0;
-    ct = 0;
-    if (s[i] != c && s[i] != '\0')
-        ct++;
-    i++;
-    while (s[i] != '\0')
-    {
-        if (s[i] != c && s[i - 1] == c)
-            ct++;
+        if (!s || !c)
+                return (0);
+        i = 0;
+        ct = 0;
+        if (s[i] != c && s[i] != '\0')
+                ct++;
         i++;
-    }
-    return (ct);
-}
-
-char *ft_mallocword(char const *s, char c)
-{
-    char *word;
-    char *start;
-    size_t j;
-    size_t i;
-
-    start = s;
-    i = 0;
-    j = 0;
-    while (*s != c && *s != '\0')
-    {
-        s++;
-        i++;
-    }
-    word = (char *)malloc(sizeof(char) * (i + 1));
-    if (word == NULL)
-        return (NULL);
-    while (*start != c && *start != '\0')
-    {
-        word[j] = *start;
-        j++;
-        start++;
-    }
-    word[j] = '\0';
-    return (word);
-}
-
-char **ft_split(char const *s, char c)
-{
-    char **arr;
-    char *word;
-    size_t words;
-    size_t i;
-    size_t j;
-
-    i = 0;
-    j = 0;
-    words = ft_count_words(s, c);
-    arr = (char **)malloc(sizeof(char *) * (words + 1));
-    if (arr == NULL)
-        return (NULL);
-    while (s[i] != '\0')
-    {
-        while (s[i] == c)
-            i++;
-        if (s[i] == '\0')
-            break;
-        word = ft_mallocword(&s[i], c);
-        if (word == NULL)
+        while (s[i] != '\0')
         {
-            while (j > 0)
-                free(arr[--j]);
-            free(arr);
-            return (NULL);
+                if (s[i] != c && s[i - 1] == c)
+                        ct++;
+                i++;
         }
-        arr[j] = word;
-        j++;
+        return (ct);
+}
+
+static char     *ft_mallocword(char const *s, char c)
+{
+        char    *word;
+        size_t  i;
+        size_t  j;
+
+        start = s;
+        i = 0;
+        j = 0;
         while (s[i] != c && s[i] != '\0')
-            i++;
-    }
-    arr[j] = NULL;
-    return (arr);
+                i++;
+        word = (char *) malloc(sizeof(char) * (i + 1));
+        if (word == NULL)
+                return (NULL);
+        while (j < i)
+        {
+                word[j] = s[j];
+                j++;
+        }
+        word[j] = '\0';
+        return (word);
+}
+
+char    **ft_split(char const *s, char c)
+{
+        char    **arr;
+        char    *word;
+        size_t  i;
+        size_t  j;
+
+        i = 0;
+        j = 0;
+        arr = (char **) malloc(sizeof(char *) * (ft_count_words(s, c) + 1));
+        if (arr == NULL || s == NULL || c == NULL)
+                return (NULL);
+        while (s[i] != '\0')
+        {
+                if (s[i] != c)
+                {
+                        arr[j] = ft_mallocword(&s[i], c);
+                        while (s[i] != '\0' && s[i] != c)
+                                i++;
+                        j++;
+                }
+                else if (s[i] == c)
+                        i++;
+        }
+        arr[j] = NULL;
+        return (arr);
 }
