c363c9a0bbb9cdfaed183d71da0444e2b9cef42e
Stephen Tee <stephentee88@gmail.com>
2025-10-17 13:46:14 +0800

    Update ft_strdup.c

--- libft/LIBC Functions/ft_strdup.c
+++ libft/LIBC Functions/ft_strdup.c
@@ -1,23 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stee <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/17 13:44:39 by stee              #+#    #+#             */
+/*   Updated: 2025/10/17 13:46:01 by stee             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 char    *ft_strdup(const char *s)
 {
-    size_t len;
-    size_t i;
-    char    *dup;
+        size_t  len;
+        size_t  i;
+        char    *dup;
 
-    if (s == NULL)
-        return (NULL);
-    len = ft_strlen(s);
-    i = 0;
-    dup = (char *) malloc(sizeof(char) * (len + 1));
-    if (dup == NULL)
-        return (NULL);
-    while (s[i] != '\0')
-    {
-        dup[i] = s[i];
-        i++;
-    }
-    dup[i] = '\0';
-    return (dup);
+        if (s == NULL)
+                return (NULL);
+        len = ft_strlen(s);
+        i = 0;
+        dup = (char *) malloc(sizeof(char) * (len + 1));
+        if (dup == NULL)
+                return (NULL);
+        while (s[i] != '\0')
+        {
+                dup[i] = s[i];
+                i++;
+        }
+        dup[i] = '\0';
+        return (dup);
 }
