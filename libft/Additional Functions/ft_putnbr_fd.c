5c13e6370fd895e2f8ac2509ed3d8dd606e5abee
Stephen Tee <stephentee88@gmail.com>
2025-10-17 11:37:07 +0800

    Update ft_putnbr_fd.c

--- libft/Additional Functions/ft_putnbr_fd.c
+++ libft/Additional Functions/ft_putnbr_fd.c
@@ -1,22 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stee <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/17 11:34:11 by stee              #+#    #+#             */
+/*   Updated: 2025/10/17 11:36:55 by stee             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void    ft_putnbr_fd(int n, int fd)
 {
-    long long LL;
+        long long       ll;
 
-    LL = n;
-    if (LL < 0)
-    {
-        ft_putchar_fd('-', fd);
-        LL *= -1;
-    }
-    if (LL >= 10)
-    {
-        ft_putnbr_fd(LL / 10, fd);
-        ft_putchar_fd(((LL % 10) + '0'), fd)
-    }
-    else
-    {
-        ft_putchar_fd((LL + '0'), fd);
-    }
+        ll = n;
+        if (ll < 0)
+        {
+                ft_putchar_fd('-', fd);
+                ll *= -1;
+        }
+        if (ll >= 10)
+        {
+                ft_putnbr_fd((ll / 10), fd);
+                ft_putchar_fd(((ll % 10) + '0'), fd);
+        }
+        else
+                ft_putchar_fd((ll + '0'), fd);
 }
